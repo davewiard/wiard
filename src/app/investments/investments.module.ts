37bd1e06f3a2c26ffc325a0e2404cffe0820bd66
Dave Wiard <dave.wiard@gmail.com>
2018-10-27 23:02:47 -0700

    Added empty Transaction components

--- src/app/investments/investments.module.ts
+++ src/app/investments/investments.module.ts
@@ -5,12 +5,19 @@ import { InvestmentsComponent } from './investments.component';
 import { OverviewComponent } from './overview/overview.component';
 import { TransactionsComponent } from './transactions/transactions.component';
 import { NewTransactionComponent } from './new-transaction/new-transaction.component';
+import { EditTransactionComponent } from './edit-transaction/edit-transaction.component';
 
 @NgModule({
   imports: [
     CommonModule,
     InvestmentsRoutingModule
   ],
-  declarations: [InvestmentsComponent, OverviewComponent, TransactionsComponent, NewTransactionComponent]
+  declarations: [
+    InvestmentsComponent,
+    OverviewComponent,
+    TransactionsComponent,
+    NewTransactionComponent,
+    EditTransactionComponent
+  ]
 })
 export class InvestmentsModule { }
